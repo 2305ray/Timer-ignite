f1e5c387cc9bd78e36cda9c4a058a37416da8d2f
2305ray <rayssagarcia035@gmail.com>
2025-01-28 10:29:27 -0300

    começo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './Components/styles/global'
+import { defaultTheme } from './Components/styles/themes/default'
+import { Router } from './Components/router'
+import { BrowserRouter } from 'react-router-dom'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
---
+++ src/Components/header/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+
+export  const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    nav {
+        display: flex;
+        gap: 0.5rem;
+
+        a{
+            width: 3rem;
+            height: 3rem;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            color: ${(props) => props.theme['gray-100']};
+
+            border-top: 3px solid transparent; /*ele volta pro centro */
+            border-bottom: 3px solid transparent;
+
+            &:hover {
+                border-bottom: 3px solid ${(props) => props.theme['green-500']};
+                }
+
+            &.active {
+                color: ${props => props.theme['green-500']}; /* Mudança de cor quando ativo */
+                }
+
+                /* Remover outline também quando o link tiver foco */
+            &:focus {
+                outline: none;
+                box-shadow: none;
+            }
+            
+        }
+    }
+
+
+`;
